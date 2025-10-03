73cf13d79f06f9de133965ab9fb5bd3b9648e153
alinabodnarpn <bodnar.pn@ucu.edu.ua>
2025-10-03 12:52:09 +0300

    Delete MoreOOP-main directory

--- MoreOOP-main/src/main/java/kicks/KickStrategy.java
+++
@@ -1,7 +0,0 @@
-package kicks;
-
-import lotr.Character;
-
-public interface KickStrategy {
-    void kick(Character attacker, Character opponent);
-}
--- MoreOOP-main/src/main/java/lotr/Elf.java
+++
@@ -1,9 +0,0 @@
-package lotr;
-
-import kicks.ElfKick;
-
-public class Elf extends Character {
-    public Elf() {
-        super(10, 10, new ElfKick());
-    }
-}
